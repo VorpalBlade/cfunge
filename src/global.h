7e0ec6574de6b7b06056c7a38f80d871dcf26473
Arvid Norlander <code@vorpal.se>
2008-10-30 14:24:09 +0100

    cfunge 0.3.3 release time.

--- src/global.h
+++ src/global.h
@@ -162,9 +162,9 @@ typedef uint32_t fungeUnsignedCell;
 #  error "Please rename FUNGEHANDPRINT to FUNGE_OLD_HANDPRINT and add FUNGE_NEW_HANDPRINT!"
 #endif
 /// Version, for -V.
-#define APPVERSION  "0.3.2"
+#define APPVERSION  "0.3.3"
 /// For y instruction.
-#define FUNGEVERSION 32
+#define FUNGEVERSION 33
 
 // Define if you use fuzz testing script.
 //#define FUZZ_TESTING
