04151548ccd4efecf2f59fb4bae28437921f2f48
Arvid Norlander <code@vorpal.se>
2009-04-08 12:13:56 +0200

    Add system for diagnostics output, collected in one place.

---
+++ src/diagnostic.c
@@ -0,0 +1,77 @@
+/* -*- mode: C; coding: utf-8; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
+ *
+ * cfunge - A standard-conforming Befunge93/98/109 interpreter in C.
+ * Copyright (C) 2008-2009 Arvid Norlander <anmaster AT tele2 DOT se>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at the proxy's option) any later version. Arvid Norlander is a
+ * proxy who can decide which future versions of the GNU General Public
+ * License can be used.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "global.h"
+#include "diagnostic.h"
+#include <stdio.h>
+
+FUNGE_ATTR_FAST
+void diag_fatal(const char* message)
+{
+	fprintf(stderr, "FATAL: %s\n", message);
+	exit(1);
+}
+
+FUNGE_ATTR_FAST
+void diag_error(const char* message)
+{
+	fprintf(stderr, "ERROR: %s\n", message);
+}
+
+FUNGE_ATTR_FAST
+void diag_warn(const char* message)
+{
+	fprintf(stderr, "WARN: %s\n", message);
+}
+
+
+FUNGE_ATTR_FAST
+void diag_fatal_format(const char* format, ...)
+{
+	va_list ap;
+	fputs("FATAL: ", stderr);
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+FUNGE_ATTR_FAST
+void diag_error_format(const char* format, ...)
+{
+	va_list ap;
+	fputs("ERROR: ", stderr);
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	fputc('\n', stderr);
+}
+
+FUNGE_ATTR_FAST
+void diag_warn_format(const char* format, ...)
+{
+	va_list ap;
+	fputs("WARN: ", stderr);
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	fputc('\n', stderr);
+}
---
+++ src/diagnostic.h
@@ -0,0 +1,146 @@
+/* -*- mode: C; coding: utf-8; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
+ *
+ * cfunge - A standard-conforming Befunge93/98/109 interpreter in C.
+ * Copyright (C) 2008-2009 Arvid Norlander <anmaster AT tele2 DOT se>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at the proxy's option) any later version. Arvid Norlander is a
+ * proxy who can decide which future versions of the GNU General Public
+ * License can be used.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * Diagnostics handling.
+ */
+
+#ifndef FUNGE_HAD_SRC_DIAGNOSTIC_H
+#define FUNGE_HAD_SRC_DIAGNOSTIC_H
+
+#include "global.h"
+#include <stdarg.h>
+
+/**
+ * @defgroup diagnostics Diagnostics
+ * Various functions and macros for error reporting.
+ */
+/*@{*/
+/// For internal use in this header only.
+#define DIAG_SOURCELOC "[" __FILE__ ":" FUNGE_CPP_STRINGIFY(__LINE__) "]"
+
+/// Print out of memory message and abort().
+#define DIAG_OOM(m_reason) \
+	do { \
+		fputs("FATAL: Out of memory at " DIAG_SOURCELOC ":\n " m_reason "\n", stderr); \
+		abort(); \
+	} while(0)
+
+/**
+ * Like diag_fatal() but includes file and line number, meant for errors that
+ * should never happen (not for user typoing something).
+ */
+#define DIAG_FATAL_LOC(m_message) \
+	do { \
+		fputs("FATAL " DIAG_SOURCELOC ": " m_message "\n", stderr); \
+		exit(EXIT_FAILURE); \
+	} while(0)
+
+/**
+ * Like diag_error() but includes file and line number, meant for errors that
+ * should never happen (not for user typoing something).
+ */
+#define DIAG_ERROR_LOC(m_message) \
+	fputs("ERROR " DIAG_SOURCELOC ": " m_message "\n", stderr); \
+
+/**
+ * Like diag_warn() but includes file and line number, meant for errors that
+ * should never happen (not for user typoing something).
+ */
+#define DIAG_WARN_LOC(m_fmt) \
+	fputs("WARN " DIAG_SOURCELOC ": " m_message "\n", stderr); \
+
+
+/**
+ * Like diag_fatal_format() but includes file and line number, meant for errors
+ * that should never happen (not for user typoing something).
+ */
+#define DIAG_FATAL_FORMAT_LOC(m_fmt, ...) \
+	do { \
+		fprintf(stderr, "FATAL " DIAG_SOURCELOC ": " m_fmt "\n", __VA_ARGS__); \
+		exit(EXIT_FAILURE); \
+	} while(0)
+
+/**
+ * Like diag_error_format() but includes file and line number, meant for errors
+ * that should never happen (not for user typoing something).
+ */
+#define DIAG_ERROR_FORMAT_LOC(m_fmt, ...) \
+	fprintf(stderr, "ERROR " DIAG_SOURCELOC ": " m_fmt "\n", __VA_ARGS__); \
+
+/**
+ * Like diag_warn_format() but includes file and line number, meant for errors
+ * that should never happen (not for user typoing something).
+ */
+#define DIAG_WARN_FORMAT_LOC(m_fmt, ...) \
+	fprintf(stderr, "WARN " DIAG_SOURCELOC ": " m_fmt "\n", __VA_ARGS__); \
+
+
+
+/**
+ * Prints "FATAL: message" and exits.
+ * @param message Message to print
+ */
+FUNGE_ATTR_FAST FUNGE_ATTR_COLD FUNGE_ATTR_NONNULL FUNGE_ATTR_NORET
+void diag_fatal(const char* message);
+
+/**
+ * Prints "ERROR: message" and returns.
+ * @param message Message to print
+ */
+FUNGE_ATTR_FAST FUNGE_ATTR_COLD FUNGE_ATTR_NONNULL
+void diag_error(const char* message);
+
+/**
+ * Prints "WARN: message" and returns.
+ * @param message Message to print
+ */
+FUNGE_ATTR_FAST FUNGE_ATTR_COLD FUNGE_ATTR_NONNULL
+void diag_warn(const char* message);
+
+/**
+ * Prints "FATAL: formatted message" and exits.
+ * @param format Format string to use.
+ */
+FUNGE_ATTR_FAST FUNGE_ATTR_COLD FUNGE_ATTR_NONNULL FUNGE_ATTR_NORET
+FUNGE_ATTR_FORMAT(printf,1,2)
+void diag_fatal_format(const char* format, ...);
+
+/**
+ * Prints "ERROR: formatted message" and returns.
+ * @param format Format string to use.
+ */
+FUNGE_ATTR_FAST FUNGE_ATTR_COLD FUNGE_ATTR_NONNULL
+FUNGE_ATTR_FORMAT(printf,1,2)
+void diag_error_format(const char* format, ...);
+
+/**
+ * Prints "WARN: formatted message" and returns.
+ * @param format Format string to use.
+ */
+FUNGE_ATTR_FAST FUNGE_ATTR_COLD FUNGE_ATTR_NONNULL
+FUNGE_ATTR_FORMAT(printf,1,2)
+void diag_warn_format(const char* format, ...);
+
+/*@}*/
+
+#endif
