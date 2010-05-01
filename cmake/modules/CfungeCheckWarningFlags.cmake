f820eeda1da31a818779ae568f7e3cbea03ca644
Arvid Norlander <code@vorpal.se>
2010-05-01 14:34:19 +0200

    Some build system fixes to make it more clang-friendly.

--- cmake/modules/CfungeCheckWarningFlags.cmake
+++ cmake/modules/CfungeCheckWarningFlags.cmake
@@ -26,53 +26,55 @@ include(CfungeCheckCflag)
 
 macro(CFUNGE_CHECK_WARNING_FLAGS)
 	if (CMAKE_COMPILER_IS_GNUCC)
+		# Use -Werror so we error out on "warning: unknown flag" and such.
+		# However since the test file cmake uses uses int main() instead of
+		# int main(void) we need to test two of the flags after all other ones
+		set(CMAKE_REQUIRED_FLAGS "-Werror")
 		# Not all GCC versions support all of these flags.
 		CFUNGE_CHECK_CFLAG(Wall                         -Wall)
 		CFUNGE_CHECK_CFLAG(Wextra                       -Wextra)
 		CFUNGE_CHECK_CFLAG(pedantic                     -pedantic)
 
-		CFUNGE_CHECK_CFLAG(Wwrite-strings               -Wwrite-strings)
+		CFUNGE_CHECK_CFLAG(Wwrite_strings               -Wwrite-strings)
 
-		CFUNGE_CHECK_CFLAG(Wcast-align                  -Wcast-align)
-		CFUNGE_CHECK_CFLAG(Wcast-qual                   -Wcast-qual)
-		CFUNGE_CHECK_CFLAG(Wbad-function-cast           -Wbad-function-cast)
+		CFUNGE_CHECK_CFLAG(Wcast_align                  -Wcast-align)
+		CFUNGE_CHECK_CFLAG(Wcast_qual                   -Wcast-qual)
+		CFUNGE_CHECK_CFLAG(Wbad_function_cast           -Wbad-function-cast)
 
-		CFUNGE_CHECK_CFLAG(Wstrict-prototypes           -Wstrict-prototypes)
-		CFUNGE_CHECK_CFLAG(Wmissing-prototypes          -Wmissing-prototypes)
-		CFUNGE_CHECK_CFLAG(Wmissing-declarations        -Wmissing-declarations)
-		CFUNGE_CHECK_CFLAG(Wold-style-definition        -Wold-style-definition)
+		CFUNGE_CHECK_CFLAG(Wmissing_prototypes          -Wmissing-prototypes)
+		CFUNGE_CHECK_CFLAG(Wmissing_declarations        -Wmissing-declarations)
 
-		CFUNGE_CHECK_CFLAG(Wredundant-decls             -Wredundant-decls)
-		CFUNGE_CHECK_CFLAG(Wnested-externs              -Wnested-externs)
-		CFUNGE_CHECK_CFLAG(Wdeclaration-after-statement -Wdeclaration-after-statement)
+		CFUNGE_CHECK_CFLAG(Wredundant_decls             -Wredundant-decls)
+		CFUNGE_CHECK_CFLAG(Wnested_externs              -Wnested-externs)
+		CFUNGE_CHECK_CFLAG(Wdeclaration_after_statement -Wdeclaration-after-statement)
 
 		CFUNGE_CHECK_CFLAG(Wshadow                      -Wshadow)
 		CFUNGE_CHECK_CFLAG(Wundef                       -Wundef)
 		CFUNGE_CHECK_CFLAG(Wpacked                      -Wpacked)
-		CFUNGE_CHECK_CFLAG(Wfloat-equal                 -Wfloat-equal)
+		CFUNGE_CHECK_CFLAG(Wfloat_equal                 -Wfloat-equal)
 
-		CFUNGE_CHECK_CFLAG(Wstrict-aliasing_2           -Wstrict-aliasing=2)
+		CFUNGE_CHECK_CFLAG(Wstrict_aliasing_2           -Wstrict-aliasing=2)
 		#CFUNGE_CHECK_CFLAG(Wstrict-overflow_5          -Wstrict-overflow=5)
 		CFUNGE_CHECK_CFLAG(Wformat_2                    -Wformat=2)
 
-		CFUNGE_CHECK_CFLAG(Wmissing-noreturn            -Wmissing-noreturn)
-		CFUNGE_CHECK_CFLAG(Wmissing-format-attribute    -Wmissing-format-attribute)
+		CFUNGE_CHECK_CFLAG(Wmissing_noreturn            -Wmissing-noreturn)
+		CFUNGE_CHECK_CFLAG(Wmissing_format_attribute    -Wmissing-format-attribute)
 
-		CFUNGE_CHECK_CFLAG(Winit-self                   -Winit-self)
+		CFUNGE_CHECK_CFLAG(Winit_self                   -Winit-self)
 		# GCC 4.1(?) or later
-		CFUNGE_CHECK_CFLAG(Wunsafe-loop-optimizations   -Wunsafe-loop-optimizations)
-		CFUNGE_CHECK_CFLAG(Wmissing-include-dirs        -Wmissing-include-dirs)
+		CFUNGE_CHECK_CFLAG(Wunsafe_loop_optimizations   -Wunsafe-loop-optimizations)
+		CFUNGE_CHECK_CFLAG(Wmissing_include_dirs        -Wmissing-include-dirs)
 
-		CFUNGE_CHECK_CFLAG(Wunused-parameter            -Wunused-parameter)
+		CFUNGE_CHECK_CFLAG(Wunused_parameter            -Wunused-parameter)
 		# These are part of -Wall in GCC 4.3, but not in some older versions.
-		CFUNGE_CHECK_CFLAG(Wunused-function             -Wunused-function)
-		CFUNGE_CHECK_CFLAG(Wunused-label                -Wunused-label)
-		CFUNGE_CHECK_CFLAG(Wunused-value                -Wunused-value)
-		CFUNGE_CHECK_CFLAG(Wunused-variable             -Wunused-variable)
+		CFUNGE_CHECK_CFLAG(Wunused_function             -Wunused-function)
+		CFUNGE_CHECK_CFLAG(Wunused_label                -Wunused-label)
+		CFUNGE_CHECK_CFLAG(Wunused_value                -Wunused-value)
+		CFUNGE_CHECK_CFLAG(Wunused_variable             -Wunused-variable)
 		CFUNGE_CHECK_CFLAG(Wimplicit                    -Wimplicit)
 		CFUNGE_CHECK_CFLAG(Wparentheses                 -Wparentheses)
 		# Part of -pedantic in 4.3
-		CFUNGE_CHECK_CFLAG(Wpointer-arith               -Wpointer-arith)
+		CFUNGE_CHECK_CFLAG(Wpointer_arith               -Wpointer-arith)
 
 		# Maintainer flags. Gives lots of false positives.
 		# add_definitions(-Wunreachable-code -fno-inline
@@ -82,5 +84,9 @@ macro(CFUNGE_CHECK_WARNING_FLAGS)
 		#add_definitions(-Wunreachable-code)
 		# Only with this meaning in GCC 4.3 (and false positives):
 		# -Wconversion
+		unset(CMAKE_REQUIRED_FLAGS)
+		# Test those where GCC gives warning on the test file cmake uses.
+		CFUNGE_CHECK_CFLAG(Wstrict_prototypes           -Wstrict-prototypes)
+		CFUNGE_CHECK_CFLAG(Wold_style_definition        -Wold-style-definition)
 	endif (CMAKE_COMPILER_IS_GNUCC)
 endmacro(CFUNGE_CHECK_WARNING_FLAGS)
