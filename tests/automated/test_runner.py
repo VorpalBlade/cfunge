d3fdc3898a3bba5787bde26af670a587b87bb9b3
Arvid Norlander <code@vorpal.se>
2017-03-19 13:39:00 +0100

    Give up on Travis CI and Python 3.4. Port script to Python 3.2. Blergh.

--- tests/automated/test_runner.py
+++ tests/automated/test_runner.py
@@ -2,34 +2,34 @@
 """Simple test runner for cfunge"""
 
 import argparse
+import os
 import sys
 import subprocess
 
-from pathlib import Path
-
-
 _SUFFIX_MAP = {
-    '.b109': '109',
-    '.b93': '93',
-    '.b98': '98',
-    '.bf': '93',
+    'b109': '109',
+    'b93': '93',
+    'b98': '98',
+    'bf': '93',
 }
+
 
 def main():
     """Main function"""
     parser = argparse.ArgumentParser(description='Test runner for cfunge')
     parser.add_argument('cfunge_path',
-                        type=Path,
                         help='Path to cfunge')
     parser.add_argument('test_file',
-                        type=Path,
                         help='Path to test file')
     args = parser.parse_args()
-    test = args.test_file  # type: Path
-    output = subprocess.check_output([str(args.cfunge_path),
-                                      '-s', _SUFFIX_MAP[test.suffix],
-                                      str(test)])
-    with test.with_suffix('.expected').open(mode='rb') as expected_file:
+    test = args.test_file
+    test_extension = test.split('.')[-1]
+    expected_file_path = '.'.join(test.split('.')[:-1]) + '.expected'
+    output = subprocess.check_output([args.cfunge_path,
+                                      '-s', _SUFFIX_MAP[test_extension],
+                                      test])
+
+    with open(expected_file_path, mode='rb') as expected_file:
         expected = expected_file.read()
         if expected == output:
             sys.exit(0)
