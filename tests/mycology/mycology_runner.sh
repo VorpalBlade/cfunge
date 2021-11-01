b1b6c2bb07cf8c4b97561996f5b0074ea2289a67
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2021-11-01 14:55:24 +0100

    Add checks for basic programs
    After tests in an Alpine docker container that lacked these by default.

--- tests/mycology/mycology_runner.sh
+++ tests/mycology/mycology_runner.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Runner for mycology
-# $1 Path to python runner
-# $2 Path to cfunge
-# $3 Path to mycology directory (where this file resides)
+# $1 Path to Python
+# $2 Path to python runner
+# $3 Path to cfunge
+# $4 Path to mycology directory (where this file resides)
 
-cp "$3"/src/*.b98 "$3"/src/*.bf "$3"/*.expected .
+PYTHON="$1"
+RUNNER="$2"
+CFUNGE="$3"
+MYCOLOGY="$4"
 
-"$1" "$2" mycology.b98 "$3/mycology_output_filter.sh" --exit-code 15 || exit 1
+cp "$MYCOLOGY"/src/*.b98 "$MYCOLOGY"/src/*.bf "$MYCOLOGY"/*.expected .
 
-echo -e "1\nx\n7\n16\nabc\n" | "$1" "$2" mycouser.b98 || exit 1
+"$PYTHON" "$RUNNER" "$CFUNGE" mycology.b98 "$MYCOLOGY/mycology_output_filter.sh" --exit-code 15 || exit 1
+
+echo -e "1\nx\n7\n16\nabc\n" | "$PYTHON" "$RUNNER" "$CFUNGE" mycouser.b98 || exit 1
