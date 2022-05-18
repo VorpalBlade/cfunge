2bb50596597aaf0ba4f4e5761cfe045188ba5d3d
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2022-05-18 14:10:58 +0200

    Fix astyle.sh

--- tools/astyle.sh
+++ tools/astyle.sh
@@ -2,4 +2,20 @@
 # This is used to format files in cfunge as needed. You need to install astyle to use this:
 #   http://astyle.sourceforge.net/
 #   I use astyle 2.01. No idea if it will work with previous versions.
-astyle --indent-preprocessor --indent-namespaces --indent-labels --keep-one-line-statements --keep-one-line-blocks --indent=tab=4 --max-instatement-indent=40 --brackets=linux --min-conditional-indent=1 --unpad-paren --indent-switches --pad-header --pad-oper "$@"
+
+args=(
+	--indent-preprocessor
+	--indent-namespaces
+	--indent-labels
+	--keep-one-line-statements
+	--keep-one-line-blocks
+	--indent=tab=4
+	--max-instatement-indent=40
+	--min-conditional-indent=1
+	--unpad-paren
+	--indent-switches
+	--pad-header
+	--pad-oper
+)
+
+astyle "${args[@]}" "$@"
